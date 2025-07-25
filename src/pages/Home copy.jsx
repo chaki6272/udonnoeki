9a7e222f89ca5322392a13a96974ccaaae0d67b2
anonymous <anonymous@example.com>
2025-07-25 18:26:34 +0900

    アプリ化の続き

---
+++ src/pages/Home copy.jsx
@@ -0,0 +1,20 @@
+// 例：App.jsx や BarcodeScannerComponent.jsx など
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+
+
+function Home() {
+  return (
+     <div className="page-content">
+      <h1>うどんの駅 西桂店</h1>
+      <p>富士山の湧水を使った自家製麺。味噌・醤油のつゆからお選びいただけます。</p>
+      <h2>営業時間</h2>
+      <p>11:00～20:00（定休日：1月1日）</p>
+      <h2>住所</h2>
+      <p>山梨県南都留郡西桂町小沼1112-1</p>
+      <h2>連絡先</h2>
+      <p>電話：0555-29-2040<br/>メール：udon@kk-tsuruta.jp</p>
+    </div>
+  );
+}
+
+export default Home;
